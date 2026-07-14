6a07938cdf77fd0641a897ab1b12ce0be13e629b
tanishp1 <pawartanish73@gmail.com>
2026-07-14 17:33:20 +0530

    object done

---
+++ Object/script.js
@@ -0,0 +1,25 @@
+// const car = {
+//     brand: "Toyota",
+//     model: "Corolla",
+//     start: function(){
+//         console.log(`${this.brand} ${this.model}...strat`)
+//     }
+// }
+// car.start();
+
+// 1.Encapsulation
+
+// class BankAccount {
+//     #balance = 0;
+
+//     deposit(amount){
+//         this.#balance += amount;
+//     }
+
+//     getBalance(){
+//         return this.#balance;
+//     }
+// }
+// const acc = new BankAccount();
+// acc.deposit(500)
+// console.log(acc.getBalance());
